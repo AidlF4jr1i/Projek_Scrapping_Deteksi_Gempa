cf2c5e97fcc2c773b1b4fd8fb8dab0e06d58ed9d
AidlF4jr1i <ahmadaidilfajri0@gmail.com>
2025-06-20 20:25:53 +0700

    Update Main Program

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 def ektraksi_data():
-    pass
+    """
+    Tampilan Data hasil Ekstrak :
+
+    Tanggal     : 21 Juni 2025
+    Waktu       : 20:18:50 WIB
+    Magnitudo   : 4.0
+    Kedalaman   : 40 km
+    Lokasi      : LS=1.48 Bt:134.01
+    Pusat Gempa : Pusat gempa berada di laut 6 km barat daya Amalatu, Seram Bagian Barat
+    Dirasakan   : Dirasakan (Skala MMI): II-III Ambon, II-III Saparua, II-III Amahai
+    """
+    hasil = dict()
+    hasil['tanggal'] = ['21 Juni 2025']
+    hasil['waktu']  = ['20:18:50 WIB']
+    print(hasil)
+    return hasil
 
 def tampilkan_data(result):
     pass
