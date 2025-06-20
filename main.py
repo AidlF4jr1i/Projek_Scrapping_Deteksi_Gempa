96937e6d260ef5b63069578b3c05cf3ffd6b79b8
AidlF4jr1i <ahmadaidilfajri0@gmail.com>
2025-06-20 12:14:05 +0700

    Add main program

---
+++ main.py
@@ -0,0 +1,10 @@
+def ektraksi_data():
+    pass
+
+def tampilkan_data(result):
+    pass
+
+if __name__ == '__main__':
+    print("Aplikasi Utama")
+    result = ektraksi_data()
+    tampilkan_data(result)
